40143a8113743f5f07841bf6f5c4ddf98c5d4a0e
lHeidbreder <lukas@boeke.email>
2026-01-22 13:27:58 +0100

    meta files
    added license & build script

---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#PREP
+set -e
+if [ ! -d dist/ ]; then mkdir dist; fi
+
+desktop () {
+    DESKTOP_PLATFORMS="linux-x64 win-x64"
+
+    for p in $DESKTOP_PLATFORMS
+    do
+        echo Desktop: $p
+        dotnet publish \
+            -c Release \
+            -r $p \
+            -o dist/$p \
+            dsa_stattracker_xplat.Desktop/
+    done
+}
+
+android () {
+    echo Android
+    dotnet publish \
+        -c Release \
+        -o dist/android \
+        dsa_stattracker_xplat.Android/
+}
+
+wasm () {
+    echo WASM
+    dotnet publish \
+        -c Release \
+        -p:Configuration=Release \
+        -o dist/wasm \
+        dsa_stattracker_xplat.Browser/
+}
+
+#PERFORM
+desktop
+android
+wasm
+
+echo Successfully performed!
